9ac97c2dabf166ae14e7c43c5d59209dffaf8ff4
Abdul Said <abdulrahman.asaid@gmail.com>
2021-12-10 01:45:06 -0500

    Add show on index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 import './styles.scss'
 
 export default function Appointment(props) {
+  console.log("props for appointment",props)
+  // const parsedInterviewers = props.interview.interviewers.map((interviewer) => (
+  //   interviewer.name
+    
+  // ));
   return (
-  <article className="appointment"></article>
+  <article className="appointment">
+    <Header time={props.time} />
+    {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+  </article>
   );
 }
