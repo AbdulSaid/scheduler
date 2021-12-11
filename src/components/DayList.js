ecdb338310dfc0e19a764fcb2fd0cd487e4c8290
Abdul Said <abdulrahman.asaid@gmail.com>
2021-12-11 13:39:26 -0500

    Add spots in DayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -8,6 +8,7 @@ export default function DayList(props) {
     <DayListItem
       key={day.id}
       name={day.name}
+      spots={day.spots}
       selected={day.name === props.day} 
       setDay = {props.setDay} 
     />
